24567d003e271a02b789cac56587b978346b33aa
IMRANDIL <aliimranadil2@gmail.com>
2023-01-26 19:11:29 +0530

    colors added

--- App.jsx
+++ App.jsx
@@ -3,6 +3,16 @@ import {Switch, View} from 'react-native';
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
+
+  const colors = {
+    dark: '#22252D',
+    dark1: '#292B36',
+    dark2: '#272B33',
+    light: '#FFF',
+    light1: '#F1F1F1',
+    light2: '#F7F7F7',
+  };
+
   return (
     <View>
       <Switch
